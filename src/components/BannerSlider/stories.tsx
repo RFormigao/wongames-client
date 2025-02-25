feaad42e8306acede79f66a333c647ffa89168df
Robson Formigão <rformigao.gomes@gmail.com>
2025-02-24 22:01:27 -0300

    Add BannerSlider

---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from '@storybook/react'
+import BannerSlider, { BannerSliderProps } from '.'
+
+const items = [
+  {
+    img: '/img/storybook/red-dead-img.jpg',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: '/img/storybook/red-dead-img.jpg',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: '/img/storybook/red-dead-img.jpg',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryObj<BannerSliderProps> = {
+  render: (props) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <BannerSlider {...props} />
+    </div>
+  )
+}
