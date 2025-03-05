7ff114a08d19ac568fd75c10d99bbeaebcde2f0c
Robson Formigão <rformigao.gomes@gmail.com>
2025-03-04 23:18:19 -0300

    Add Checkbox component

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -102,7 +102,7 @@ export const MenuFull = styled.nav<MenuFullProps>`
     right: 0;
     height: 100vh;
     overflow: hidden;
-    transition: opacity 0.3s ease-in-out;
+    transition: opacity ${theme.transition.fast};
     opacity: ${$isOpen ? 1 : 0};
     pointer-events: ${$isOpen ? 'all' : 'none'};
 
@@ -131,12 +131,12 @@ export const MenuFull = styled.nav<MenuFullProps>`
       margin-bottom: ${theme.spacings.small};
 
       transform: ${$isOpen ? 'translateY(0)' : 'translateY(3rem)'};
-      transition: transform 0.3s ease-in-out;
+      transition: transform ${theme.transition.fast};
     }
 
     ${RegisterBox} {
       transform: ${$isOpen ? 'translateY(0)' : 'translateY(3rem)'};
-      transition: transform 0.3s ease-in-out;
+      transition: transform ${theme.transition.fast};
     }
   `}
 `
